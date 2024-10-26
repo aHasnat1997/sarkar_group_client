a300f132999b685b626de0ec17522dc5b9550adc
aHasnat <hasnat56703@gmail.com>
2024-10-27 04:24:18 +0600

    ADD: Login, Forgot Password and Set New Password page.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,40 @@
+'use client';
+
 import assets from "@/assets";
-import { Box, Button, Checkbox, FormControlLabel, Stack, TextField, Typography } from "@mui/material";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/form";
+import { Box, Button, Checkbox, FormControlLabel, Stack, Typography } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
+// import { useRouter } from "next/navigation";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const userZodSchema = z.object({
+  email: z.string().email({ message: 'need valid email' }),
+  password: z.string().min(8, 'Minimum 8 character.'),
+  remember: z.boolean()
+});
+
+type FormValues = z.infer<typeof userZodSchema>;
 
 export default function LoginPage() {
+  // const router = useRouter();
+
+  const methods = useForm<FormValues>({
+    resolver: zodResolver(userZodSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      remember: false
+    }
+  });
+
+  const formSubmit: SubmitHandler<FormValues> = (value) => {
+    // router.push('/dashboard');
+    console.log(value);
+  }
+
   return (
     <Stack
       width='100%'
@@ -60,42 +91,77 @@ export default function LoginPage() {
             </Typography>
           </Box>
         </Box>
-        <Stack
-          flexDirection='column'
-          gap='1rem'
-        >
-          <TextField
-            label='Email Address'
-            sx={{
-              width: '60%'
-            }}
-          />
-          <TextField
-            label='Password'
-            type="password"
-            sx={{
-              width: '60%'
-            }}
-          />
-          <Stack width='60%' justifyContent='space-between'>
-            <FormControlLabel
-              value="Remember Me"
-              control={<Checkbox />}
-              label="Remember Me"
-              labelPlacement="end"
-            />
-            <Link href='/forgot-password'>
-              <Typography color="primary.main">
-                Forgot Password?
-              </Typography>
-            </Link>
-          </Stack>
-        </Stack>
-        <Link href='/dashboard'>
-          <Button sx={{ width: '60%' }}>
-            Login
-          </Button>
-        </Link>
+        <Form {...methods}>
+          <form onSubmit={methods.handleSubmit(formSubmit)}>
+            <Stack
+              flexDirection='column'
+              gap='1rem'
+            >
+              <FormItem>
+                <FormField
+                  name='email'
+                  control={methods.control}
+                  render={({ field }) => (
+                    <FormControl
+                      {...field}
+                      label='Email'
+                      sx={{
+                        width: '60%'
+                      }}
+                    />
+                  )}
+                />
+              </FormItem>
+
+              <FormItem>
+                <FormMessage />
+                <FormField
+                  name='password'
+                  control={methods.control}
+                  render={({ field }) => (
+                    <FormControl
+                      {...field}
+                      label='Password'
+                      type='password'
+                      sx={{
+                        width: '60%'
+                      }}
+                    />
+                  )}
+                />
+              </FormItem>
+
+              <Stack width='60%' justifyContent='space-between'>
+                <FormItem>
+                  <Controller
+                    name='remember'
+                    control={methods.control}
+                    render={({ field }) => (
+                      <FormControlLabel
+                        // value={true}
+                        control={<Checkbox />}
+                        label="Remember Me"
+                        labelPlacement="end"
+                        {...field}
+                      />
+                    )}
+                  />
+                </FormItem>
+                <Link href='/forgot-password'>
+                  <Typography color="primary.main">
+                    Forgot Password?
+                  </Typography>
+                </Link>
+              </Stack>
+            </Stack>
+            <Button
+              type='submit'
+              sx={{ width: '60%' }}
+            >
+              Login
+            </Button>
+          </form>
+        </Form>
       </Stack>
     </Stack>
   );
---
+++ src/app/(auth)/set-new-password/page.tsx
@@ -0,0 +1,137 @@
+'use client';
+
+import assets from "@/assets";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/form";
+
+const zodSchema = z.object({
+  newPassword: z.string().min(8, 'Minimum 8 character.'),
+  confirmPassword: z.string().min(8, 'Minimum 8 character.'),
+});
+
+type FormValues = z.infer<typeof zodSchema>;
+
+export default function SetNewPassword() {
+  const router = useRouter();
+
+  const methods = useForm<FormValues>({
+    resolver: zodResolver(zodSchema),
+    defaultValues: {
+      newPassword: '',
+      confirmPassword: ''
+    }
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const formSubmit = (value: any) => {
+    router.push('/login');
+    console.log(value);
+  }
+
+  return (
+    <Stack
+      width='100%'
+      height='100vh'
+      padding='2rem'
+      gap='3rem'
+    >
+      <Stack
+        width='100%'
+        height='100%'
+        bgcolor='rgba(18, 149, 209, 0.05)'
+        borderRadius='1.88rem'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Image
+          src={assets.logo.icon}
+          alt="logo"
+          width={1500}
+          height={1500}
+          className="size-80"
+        />
+      </Stack>
+      <Stack
+        width='100%'
+        height='100%'
+        justifyContent='center'
+        flexDirection='column'
+        gap='2rem'
+      >
+        <Link href='/login'>
+          <ArrowBackIosNewIcon />
+          Back
+        </Link>
+        <Box>
+          <Typography
+            component='h4'
+            fontSize='1.875rem'
+            fontWeight={600}
+          >
+            Set New Password
+          </Typography>
+          <Typography
+            fontSize='1rem'
+            color='text.secondary'
+            fontWeight={300}
+          >
+            To set new password. Enter new password <br /> and confirm it.
+          </Typography>
+        </Box>
+        <Form {...methods}>
+          <form onSubmit={methods.handleSubmit(formSubmit)}>
+            <Stack
+              flexDirection='column'
+              gap='1rem'
+            >
+              <FormItem>
+                <FormMessage />
+                <FormField
+                  name='newPassword'
+                  control={methods.control}
+                  render={({ field }) => (
+                    <FormControl
+                      {...field}
+                      label='New Password'
+                      type='password'
+                      sx={{
+                        width: '60%'
+                      }}
+                    />
+                  )}
+                />
+              </FormItem>
+              <FormItem>
+                <FormMessage />
+                <FormField
+                  name='confirmPassword'
+                  control={methods.control}
+                  render={({ field }) => (
+                    <FormControl
+                      {...field}
+                      label='Confirm Password'
+                      type='password'
+                      sx={{
+                        width: '60%'
+                      }}
+                    />
+                  )}
+                />
+              </FormItem>
+              <Button sx={{ width: '60%' }} type="submit">
+                Go to Login
+              </Button>
+            </Stack>
+          </form>
+        </Form>
+      </Stack>
+    </Stack>
+  );
+};
