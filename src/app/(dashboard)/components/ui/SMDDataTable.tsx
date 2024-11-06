72e60a4b82c99c293b122118c61c46598dfbf5cf
aHasnat <hasnat56703@gmail.com>
2024-11-07 02:47:52 +0600

    add: SMDDataTable

---
+++ src/app/(dashboard)/components/ui/SMDDataTable.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel, Paper, Typography, Badge, TablePagination } from '@mui/material';
+import { styled } from '@mui/system';
+
+type SortOrder = 'asc' | 'desc';
+
+type Column<T> = {
+  label: string;
+  field: keyof T | ((row: T) => React.ReactNode);  // Allows functions to combine fields
+  isSortable?: boolean;
+  badgeStatus?: boolean;
+};
+
+type TableComponentProps<T> = {
+  data: T[];
+  columns: Column<T>[];
+  page: number;
+  limit: number;
+  total?: number;
+  totalPages: number;
+  sortBy?: keyof T;
+  sortOrder?: SortOrder;
+  onSortChange?: (field: keyof T) => void;
+  onPageChange: (page: number) => void;
+  onLimitChange: (page: number) => void;
+};
+
+const BadgeStyled = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-dot': {
+    height: 8,
+    minWidth: 8,
+    borderRadius: '50%',
+    backgroundColor: theme.palette.info.main,
+  },
+}));
+
+export default function SMDDataTable<T>({
+  data,
+  columns,
+  page,
+  limit,
+  // total,
+  totalPages,
+  sortBy,
+  sortOrder,
+  onSortChange,
+  onPageChange,
+  onLimitChange
+}: TableComponentProps<T>) {
+  const handleSort = (field: keyof T) => {
+    if (onSortChange) onSortChange(field);
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {columns.map((col, index) => (
+              <TableCell key={index}>
+                {col.isSortable ? (
+                  <TableSortLabel
+                    active={sortBy === col.field}
+                    direction={sortBy === col.field ? sortOrder : 'asc'}
+                    onClick={() => handleSort(col.field as keyof T)}
+                  >
+                    {col.label}
+                  </TableSortLabel>
+                ) : (
+                  <Typography variant="subtitle1" color='text.secondary'>{col.label}</Typography>
+                )}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {columns.map((col, colIndex) => (
+                <TableCell key={colIndex}>
+                  {typeof col.field === 'function' ? (
+                    col.field(row)
+                  ) : col.badgeStatus ? (
+                    <BadgeStyled color={row[col.field] === 'WORKING' ? 'primary' : row[col.field] === 'AVAILABLE' ? 'success' : 'error'} variant="dot">
+                      <Typography>{String(row[col.field])}</Typography>
+                    </BadgeStyled>
+                  ) : (
+                    <Typography color='text.primary'>{String(row[col.field])}</Typography> /* Convert value to string */
+                  )}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        component={'div'}
+        count={totalPages}
+        page={page}
+        onPageChange={(event, value) => onPageChange(value)}
+        rowsPerPage={limit}
+        rowsPerPageOptions={[5, 10, 25, 50]}
+        onRowsPerPageChange={(event) => onLimitChange(Number(event.target?.value))}
+        color="primary"
+      />
+    </TableContainer>
+  );
+};
