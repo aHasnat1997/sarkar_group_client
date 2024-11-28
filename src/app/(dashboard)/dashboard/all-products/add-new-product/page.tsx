88f4241701a9bdb805e694345d7b16851642a746
aHasnat <hasnat56703@gmail.com>
2024-11-28 20:01:17 +0600

    Update and Add Page Add: All Product Reques page and Add New Product page Update: All Product page.

---
+++ src/app/(dashboard)/dashboard/all-products/add-new-product/page.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+
+export default function AddNewProduct() {
+  return (
+    <Box
+      sx={{
+        border: '.5px solid',
+        borderColor: 'grey.400',
+        borderRadius: '1rem',
+        overflow: 'hidden',
+        padding: '1.5rem'
+      }}
+    >
+
+    </Box>
+  );
+};
